81eaf8333430bd7432e7184f410fc96e6ddedb25
Steen Clarke <cmclarkey@gmail.com>
2016-08-24 21:23:49 -0700

    refactor click all the numbers game

--- app/assets/javascripts/the_numbers_game.js
+++ app/assets/javascripts/the_numbers_game.js
@@ -1,16 +1,11 @@
-var clickNumbers = [];
-var comparisons = [1,2,3,4,5,6,7,8,9,10]
+var nextNumber = 1;
 function numbersGame(number) {
-  clickNumbers.push(number);
-  holder = 0;
-  if (clickNumbers.length==comparisons.length) {
-    for (i=0; i<comparisons.length; i++) {
-      if (clickNumbers[i]==comparisons[i]) {
-        holder++;
-      }
-    }
-    if (holder==10) {
+  if (number === nextNumber) {
+    nextNumber++;
+    if (number === 10) {
       console.log("You win! What a great game!");
     }
+  } else {
+    nextNumber = 1;
   }
 }
