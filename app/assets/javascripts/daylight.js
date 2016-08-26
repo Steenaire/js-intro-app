200c4a782164a9c45a5f2b748595b15df5d6d88f
Steen Clarke <cmclarkey@gmail.com>
2016-08-25 21:04:00 -0700

    Make text in daylight game large even after click

--- app/assets/javascripts/daylight.js
+++ app/assets/javascripts/daylight.js
@@ -1,18 +1,18 @@
 function daylightColor(time) {
   if (time === "morning") {
     document.body.style.backgroundColor = "pink";
-    document.getElementById("morning").innerHTML = "Morning";
+    document.getElementById("morning").innerHTML = "<h1>Morning</h1>";
     document.getElementById("afternoon").innerHTML = "";
     document.getElementById("evening").innerHTML = "";
   } else if (time === "afternoon") {
     document.body.style.backgroundColor = "blue";
     document.getElementById("morning").innerHTML = "";
-    document.getElementById("afternoon").innerHTML = "Afternoon";
+    document.getElementById("afternoon").innerHTML = "<h1>Afternoon</h1>";
     document.getElementById("evening").innerHTML = "";
   } else if (time === "evening") {
     document.body.style.backgroundColor = "black";
     document.getElementById("morning").innerHTML = "";
     document.getElementById("afternoon").innerHTML = "";
-    document.getElementById("evening").innerHTML = "Evening";
+    document.getElementById("evening").innerHTML = "<h1>Evening</h1>";
   }
 }
